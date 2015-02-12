5b79b071d0d527fece4ed0d4d190eaf17281233d
sukso96100 <sukso96100@gmail.com>
2015-02-12 20:58:00 +0900

    working on new ui

---
+++ js/initparse.js
@@ -0,0 +1,4 @@
+//Initialize Parse JS SDK
+Parse.initialize("VfbdwjDu0SrnwEo1P4kdgZAKAlUhzdFUjV05fRKk", "3LDL1LlOf5BHtnQGkosGltIluvVxYWxhbUTdeBqQ");
+var CurrentUserVar = Parse.User.current();
+var SessionToken = CurrentUserVar.getSessionToken();
