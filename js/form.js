d3896422b586b4b174dfc4cd0b195dc0b7777a9a
Youngbin Han <sukso96100@gmail.com>
2015-02-13 19:38:57 +0900

    Update form.js

--- js/form.js
+++ js/form.js
@@ -165,7 +165,9 @@ function textCounter(field,withspace) {
     var withoutspace_num = field.value.replace(/\s/g,"").length;
     var line = "공백 포함 "+withspace_num+"자 입력됨 | 공백 비포함 "+withoutspace_num+"자 입력됨";
     cntfield.value = line;
-    saveFormData()
+    if(CurrentUserVar!=null){
+ 	saveFormData()
+    }
 }
         
 //인쇄 함수
